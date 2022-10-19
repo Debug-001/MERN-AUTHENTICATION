6914109ff9bfbecf068838b3a052310a258214ba
Om Bhatt <ombhatt1158@gmail.com>
2022-10-19 21:41:45 +0530

    server side fixes

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
 	const connectionParams = {
-		useNewUrlParser: true,
+		useNewUrlParser: false,
 		useUnifiedTopology: true,
 	};
 	try {
-		mongoose.connect(process.env.DB, connectionParams);
+		mongoose.connect("mongodb+srv://dynamic:dynamicusr@dynamicusr.cfskf.mongodb.net/Data");
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
